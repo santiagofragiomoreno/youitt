1ab85d44d7f89a8cf282c9b44303da8f06075e49
santiagofragiomoreno <santiago.f@mixentradas.com>
2019-08-10 09:46:58 +0200

    #11 error authmanager en config/main

--- common/config/main.php
+++ common/config/main.php
@@ -11,6 +11,11 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            // uncomment if you want to cache RBAC items hierarchy
+            // 'cache' => 'cache',
+        ],
         'i18n' => [
             'translations' => [
                 'frontend*' => [
